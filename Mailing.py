b58118274dcf7241f39e966884447dc0fccc2469
JOYAL JOY <134049618+joyaljoykarunagappally@users.noreply.github.com>
2024-06-13 11:38:32 +0530

    Add files via upload

---
+++ Mailing.py
@@ -0,0 +1,22 @@
+import smtplib
+sender = "email@gmail.com"  #Enter the email
+password = "password"       #Enter the apppasword
+reciever = ('receiver@gmail.com')
+body = 'test email using python'
+subject = 'Testing..'
+print(reciever)
+message = f"""From:{sender}
+To:{reciever}
+Subject:{subject}\n
+{body}
+
+"""
+
+HOST = "smtp.gmail.com"
+PORT = 587
+smtp = smtplib.SMTP(HOST, PORT)
+smtp.starttls()
+smtp.login(sender,password)
+print("logged in...")
+smtp.sendmail(sender,reciever,message)
+print("email has been sent successfully")
